93078415dc36d395996229d15d124f4a5f6e7327
Dipen Lama <lamadipen@yahoo.com>
2017-10-19 21:47:45 -0500

    Uploading eureka discovery service initial configuration

---
+++ rnd-discovery-service/src/main/java/com/rnd/tut/RndDiscoveryServiceApplication.java
@@ -0,0 +1,12 @@
+package com.rnd.tut;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RndDiscoveryServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RndDiscoveryServiceApplication.class, args);
+	}
+}
